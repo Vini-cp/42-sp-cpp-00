57729926d76b851e95f609aeec725a78a1478cd5
Vinícius Cordeiro Pereira <viniciuscordeirop@gmail.com>
2022-02-13 17:39:52 +0100

    Optimize megaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,30 +6,26 @@
 /*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/13 17:11:20 by coder             #+#    #+#             */
-/*   Updated: 2022/02/13 17:30:02 by coder            ###   ########.fr       */
+/*   Updated: 2022/02/13 17:38:58 by coder            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cstring>
-#include <cctype>
 
 int main (int argc, char **argv)
 {
-	std::string megaphone = "";
-
 	if (argc > 1)
 	{
 		for (int i = 1; i < argc; i++)
 		{
 			for (size_t j = 0; j < strlen(argv[i]); j++)
-				argv[i][j] = toupper(argv[i][j]);
-			megaphone += argv[i];
+				argv[i][j] = std::toupper(argv[i][j]);
+			std::cout << argv[i];
 		}
 	}
 	else
-		megaphone = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-
-	std::cout << megaphone << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
 	return (0);
 }
