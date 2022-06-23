0a824972bb1ae6b9e8373acea4115d8bb9613e62
viniciuscordeirop@gmail.com <viniciuscordeirop@gmail.com>
2022-06-23 18:51:01 -0300

    Put ex00 on norme

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,19 +4,25 @@
 #include <iostream>
 #include <cstring>
 
-int main (int argc, char **argv)
+int main ( int argc, char **argv )
 {
-	if (argc > 1)
+	if ( argc > 1 )
 	{
-		for (int i = 1; i < argc; i++)
+		for ( int i = 1; i < argc; i++ )
 		{
-			for (size_t j = 0; j < strlen(argv[i]); j++)
-				argv[i][j] = std::toupper(argv[i][j]);
-			std::cout << argv[i];
+			for ( size_t j = 0; j < strlen( argv[ i ] ); j++ )
+            {
+                argv[ i ][ j ] = std::toupper( argv[ i ][ j ] );
+            }
+			std::cout << argv[ i ];
 		}
 	}
 	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    }
+
 	std::cout << std::endl;
+
 	return 0;
 }
