6d8433e42298e3514c79c15c74b7e7fccbd2a0c9
viniciuscordeirop@gmail.com <viniciuscordeirop@gmail.com>
2022-06-23 18:52:01 -0300

    Update git ignore

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,23 +6,23 @@
 
 int main ( int argc, char **argv )
 {
-	if ( argc > 1 )
-	{
-		for ( int i = 1; i < argc; i++ )
-		{
-			for ( size_t j = 0; j < strlen( argv[ i ] ); j++ )
+    if ( argc > 1 )
+    {
+        for ( int i = 1; i < argc; i++ )
+        {
+            for ( size_t j = 0; j < strlen( argv[ i ] ); j++ )
             {
                 argv[ i ][ j ] = std::toupper( argv[ i ][ j ] );
             }
-			std::cout << argv[ i ];
-		}
-	}
-	else
+            std::cout << argv[ i ];
+        }
+    }
+    else
     {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     }
 
-	std::cout << std::endl;
+    std::cout << std::endl;
 
-	return 0;
+    return 0;
 }
